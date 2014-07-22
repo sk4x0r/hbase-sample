00bbe012653f1d0581c9bafea0ca67418130103e
Santosh Patil <santosh.p@flipkarts-MacBook-Pro-6.local>
2014-07-22 12:43:30 +0530

    Initial commit

---
+++ src/main/java/com/flipkart/explore/hbase/PutIntoTable.java
@@ -0,0 +1,21 @@
+package com.flipkart.explore.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class PutIntoTable {
+
+	public static void main(String[] args) throws IOException{
+		Configuration conf=HBaseConfiguration.create();
+		HTable table=new HTable(conf, "testtable");
+		Put put=new Put(Bytes.toBytes("row1"));
+		put.add(Bytes.toBytes("colfam1"),Bytes.toBytes("qual1"),Bytes.toBytes("val1"));
+		put.add(Bytes.toBytes("colfam1"),Bytes.toBytes("qual2"),Bytes.toBytes("val2"));
+		table.put(put);
+	}
+}
